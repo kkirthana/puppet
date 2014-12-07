c43bb62ab19102042ec3408a13e9ab7133278527
kkirthana <kirthipala@gmail.com>
2014-12-07 00:25:23 +0000

    Finishing puppet code

--- modules/httpd/manifests/init.pp
+++ modules/httpd/manifests/init.pp
@@ -3,6 +3,16 @@ class httpd {
 	package {'httpd' :
 		ensure => latest ,
 		}
+	
+	vcsrepo { "/var/www/html":
+  		ensure => present,
+  		provider => git,
+  		source => 'https://github.com/kkirthana/WebApp.git',
+  		revision => 'master',
+		notify => Service['httpd'],
+
+		}
+	
 	service {'httpd' : 
 		ensure => running ,
 		enable => true,
